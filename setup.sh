261accd493d5480677e22af7311cefd06469ef22
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-02 11:48:05 -0700

    add cleanup and echo messages

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+echo "update packages"
 dnf upgrade --refresh
-dnf install which git gh wget unzip p -yython3-pip python3-tkinter -y
+
+echo "installing essentials"
+dnf install which git gh unzip p python3-pip python3-tkinter -y
 pip install --upgrade pip
 pip install setuptools numpy pygame
+
+echo "symlinking python"
 ln -s /usr/bin/python3 /usr/bin/python
+
+echo "setting up booksite"
 wget https://introcs.cs.princeton.edu/python/code/dist/introcs-1.0.zip
 unzip introcs-1.0.zip
 cd introcs
 python setup.py install --user
 cd ..
+
+echo "cleaning up"
 rm -rf introcs introcs-1.0.zip
+rm -rf setup.sh
