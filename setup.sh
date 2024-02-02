c38bcffec3ab14254b095be5309522f4e144306c
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-02 12:15:31 -0700

    update echo messages

--- setup.sh
+++ setup.sh
@@ -3,32 +3,32 @@
 echo ""
 echo ""
 
-echo "Setup: update packages..." -n
+echo "Setup: update packages..."
 dnf upgrade --refresh -y > /dev/null
-echo " done."
+echo "Setup: update packages... done."
 
-echo -n "Setup: installing essentials..."
+echo "Setup: installing essentials..."
 dnf install which git gh unzip python3-pip python3-tkinter -y > /dev/null
 pip install --upgrade pip > /dev/null
 pip install setuptools numpy pygame > /dev/null
-echo " done."
+echo "Setup: installing essentials... done."
 
-echo -n "Setup: symlinking python..."
+echo "Setup: symlinking python..."
 ln -s /usr/bin/python3 /usr/bin/python > /dev/null
-echo " done."
+echo "Setup: symlinking python... done."
 
-echo -n "Setup: setting up booksite..."
+echo "Setup: setting up booksite..."
 wget https://introcs.cs.princeton.edu/python/code/dist/introcs-1.0.zip 2> /dev/null
 unzip introcs-1.0.zip > /dev/null
 cd introcs-1.0 > /dev/null
 python setup.py install --user > /dev/null
 cd .. > /dev/null
-echo " done."
+echo "Setup: setting up booksite... done."
 
-echo -n "Setup: cleaning up..."
+echo "Setup: cleaning up..."
 rm -rf introcs* > /dev/null
 rm -rf setup.sh* > /dev/null
-echo " done."
+echo "Setup: cleaning up... done."
 
 echo "Finished scripted setup."
 echo ""
