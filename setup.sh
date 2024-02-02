734b9b4d57b8b298e363a768db11d54e21a6f6dc
Noah S. Roberts <35052448+Mase3206@users.noreply.github.com>
2024-02-02 12:31:10 -0700

    update echo again, hide deprecation warning

--- setup.sh
+++ setup.sh
@@ -2,33 +2,34 @@
 
 echo ""
 echo ""
+echo "Begining scripted setup."
 
-echo "Setup: update packages..."
+echo -n "Setup: update packages..."
 dnf upgrade --refresh -y > /dev/null
-echo "Setup: update packages... done."
+echo " done."
 
-echo "Setup: installing essentials..."
+echo -n "Setup: installing essentials..."
 dnf install which git gh unzip python3-pip python3-tkinter -y > /dev/null
 pip install --upgrade pip > /dev/null
 pip install setuptools numpy pygame > /dev/null
-echo "Setup: installing essentials... done."
+echo " done."
 
-echo "Setup: symlinking python..."
+echo -n "Setup: symlinking python..."
 ln -s /usr/bin/python3 /usr/bin/python > /dev/null
-echo "Setup: symlinking python... done."
+echo " done."
 
-echo "Setup: setting up booksite..."
+echo -n "Setup: setting up booksite..."
 wget https://introcs.cs.princeton.edu/python/code/dist/introcs-1.0.zip 2> /dev/null
 unzip introcs-1.0.zip > /dev/null
 cd introcs-1.0 > /dev/null
-python setup.py install --user > /dev/null
+python setup.py install --user > /dev/null 2> /dev/null
 cd .. > /dev/null
-echo "Setup: setting up booksite... done."
+echo " done."
 
-echo "Setup: cleaning up..."
+echo -n "Setup: cleaning up..."
 rm -rf introcs* > /dev/null
 rm -rf setup.sh* > /dev/null
-echo "Setup: cleaning up... done."
+echo " done."
 
 echo "Finished scripted setup."
 echo ""
